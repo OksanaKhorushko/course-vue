2b994a2c692de5cad581f41214cddf8a1366d702
OksanaKhorushko <horushkosana@gmail.com>
2022-09-24 15:16:03 +0300

    home work 2

--- test-project/src/App.vue
+++ test-project/src/App.vue
@@ -1,42 +1,72 @@
 <template>
-  <div>
+  <div id="app">
+    <img src="./assets/logo.png" alt="Vue logo">
     <div class="screen">
-      <input v-model="operand1" />
+      <input
+        v-model.number="operand1"
+        type="text"
+      />
       {{ operator }}
-      <input v-model="operand2" />
-      =
-      {{ result }}
+      <input
+        v-model.number="operand2"
+        type="text"
+      />
+      =>
+      <input
+        v-model="result"
+        type="text"
+        readonly
+      />
+      <div v-if="error" class="error">{{ error }}</div>
     </div>
-    <div class="keyoard">
-      <button @click="operator = '+'">+</button>
-      <button @click="operator = '-'">-</button>
-      <button @click="operator = '*'">*</button>
-      <button @click="operator = '/'">/</button>
-      <button @click="operator = '**'">**</button>
-      <button @click="operator = '\\'">\</button>
-      <button @click="getResult">=</button>
-
+    <div class="keyboard">
+      <div class="row">
+        <button v-for="(item, index) in operators" :key="index" @click="getResult(item)">{{ item }}</button>
+      </div>
+      <div class="numbersShown">
+        <input type="checkbox" id="checkbox" v-model="isKeyboardShown" />
+        <label for="checkbox">{{ checked }}</label>
+        <div v-if="isKeyboardShown">
+          <button v-for="(item, index) in numbers" :key="index" @click="inputValue(item)">{{ item }}</button>
+          <div class="radioBtn">
+            <input type="radio" id="operand1" value="operand1" v-model="activeInput">
+            <label for="operand1">operand1</label>
+            <input type="radio" id="operand2" value="operand2" v-model="activeInput">
+            <label for="operand2">operand2</label>
+        </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data() {
     return {
-      operand1: 0,
-      operand2: 0,
+      operand1: '',
+      operand2: '',
       operator: '+',
-      result: 0
+      result: 0,
+      error: '',
+      operators: ['+', '-', '*', '/', '**', '\\'],
+      isKeyboardShown: false,
+      checked: 'Отобразить экранную клавиатуру',
+      numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '←'],
+      activeInput: 'operand1',
     }
   },
   methods: {
-    getResult() {
-      switch(this.operator) {
+    setOperator(operator) {
+      this.operator = operator
+    },
+    getResult(operator) {
+      this.error = '';
+      this.setOperator(operator);
+      switch(operator) {
         case '+':
-          this.result = +this.operand1 + +this.operand2
+          this.result = this.operand1 + this.operand2
           break;
         case '-':
           this.result = this.operand1 - this.operand2
@@ -45,7 +75,11 @@ export default {
           this.result = this.operand1 * this.operand2
           break;
         case '/':
-          this.result = this.operand1 / this.operand2
+          if (this.operand2 === 0) {
+            this.error = 'На 0 делить нельзя'
+          } else {
+            this.result = this.operand1 / this.operand2
+          }
           break;
         case '**':
           this.result = Math.pow(this.operand1, this.operand2)
@@ -57,11 +91,40 @@ export default {
           this.result = 'Err'
           break;
       }
+    },
+    inputValue(val) {
+      const strVal = `${this[this.activeInput]}`;
+      if (val === '←') {
+        this[this.activeInput] = +(strVal.substring(0, strVal.length - 1));
+        return;
+      }
+      this[this.activeInput] = +(strVal + val);
     }
   }
 }
 </script>
 
 <style lang="scss">
+  #app {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    color: #000;
+    margin-top: 60px;
+  }
 
+  .row {
+    margin-bottom: 30px;
+  }
+
+  .error {
+    margin: 20px;
+    padding: 10px;
+    border: 1px solid red;
+    background-color: rgb(248, 175, 175);
+    color: red;
+  }
+
+  #checkbox {
+    margin-bottom: 15px;
+  }
 </style>
